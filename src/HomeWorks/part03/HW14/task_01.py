b6f0f3f6669f7e79d7933b59e95af3f961082fb7
constantin <constantin.mokin@gmail.com>
2019-04-30 11:42:29 +0300

    14_1

---
+++ src/HomeWorks/part03/HW14/task_01.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def voice(self):
+        pass
+
+
+class Pet(Animal):          #Animal
+    pass
+
+
+class WildAnimal(Animal):   #Animal
+    pass
+
+
+class FelineInterface(ABC):
+    @abstractmethod
+    def scratch(self):
+        raise NotImplemented
+
+
+class CanineInterface(ABC):
+    @abstractmethod
+    def swimm(self):
+        raise NotImplemented
+
+
+class Cat(Pet, FelineInterface):
+    def voice(self):
+        pass
+
+
+class Dog(Pet, CanineInterface):
+    def voice(self):
+        pass
+
+
+class Lion(WildAnimal, FelineInterface):
+    def voice(self):
+        pass
+
+
+class Wolf(WildAnimal, CanineInterface):
+    def voice(self):
+        pass
+
+
+#def main():
+
+
+
+#if __name__ == '__main__':
+     #main()
+
+
